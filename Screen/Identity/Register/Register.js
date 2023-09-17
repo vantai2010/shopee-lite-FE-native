ba20e3600aa44eb409bae25c4ff9baa033557c84
LaD02 <lehoanggithub10112002@gmail.com>
2023-09-17 08:40:37 +0700

    LeHuyHoang: tạo giao diện  register

--- Screen/Identity/Register/Register.js
+++ Screen/Identity/Register/Register.js
@@ -1,29 +1,32 @@
-import { ScrollView, View, Text, TextInput, Radio } from "react-native";
+import { ScrollView, View, Text, TextInput, Button } from "react-native";
 
 export default function Register () {
     return (
         <ScrollView style={{flex:1, backgroundColor:'rgba(0,0,0,.05)'}}>
             <View style={{backgroundColor:'#fff', margin:30}}>
-                <Text style={{margin:20, fontSize:16}}>FirstName</Text>
+                <Text style={{margin:20, marginBottom: 10, fontSize:16}}>FirstName</Text>
                 <TextInput  
                     style={{marginHorizontal:20, borderColor:'rgba(0,0,0,.05)', borderWidth:1, padding:12, fontSize:16}}
                 />
-                <Text style={{margin:20, fontSize:16}}>LastName</Text>
+                <Text style={{margin:20, marginBottom: 10, fontSize:16}}>LastName</Text>
                 <TextInput  
                     style={{marginHorizontal:20, borderColor:'rgba(0,0,0,.05)', borderWidth:1, padding:12, fontSize:16}}
                 /> 
-                <Text style={{margin:20, fontSize:16}}>Address</Text>
+                <Text style={{margin:20, marginBottom: 10, fontSize:16}}>Address</Text>
                 <TextInput  
                     style={{marginHorizontal:20, borderColor:'rgba(0,0,0,.05)', borderWidth:1, padding:12, fontSize:16}}
                 />
-                <Text style={{margin:20, fontSize:16}}>Phone</Text>
+                <Text style={{margin:20, marginBottom: 10, fontSize:16}}>Phone</Text>
                 <TextInput  
                     style={{marginHorizontal:20, borderColor:'rgba(0,0,0,.05)', borderWidth:1, padding:12, fontSize:16}}
                 />
-                <Text style={{margin:20, fontSize:16}}>Gentle</Text>
+                <Text style={{margin:20, marginBottom: 10, fontSize:16}}>Gentle</Text>
                 <TextInput  
                     style={{marginHorizontal:20, marginBottom:20, borderColor:'rgba(0,0,0,.05)', borderWidth:1, padding:12, fontSize:16}}
                 />
+                <View style={{backgroundColor:'#ee4d2d', marginTop:30, paddingVertical:8}}>
+                    <Button title="Đăng ký" color='#fff' />
+                </View>
             </View>
         </ScrollView>
     )
